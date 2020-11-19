fd84274ac2eb001f4c2bada1be4664b75c40c9a9
Jan.Mazurek <mazurekjan1@gmail.com>
2020-11-19 18:52:05 +0100

    Duzo zmian

--- src/main/java/com/JanCode/SKPplus/web/AdminController.java
+++ src/main/java/com/JanCode/SKPplus/web/AdminController.java
@@ -1,26 +1,45 @@
 package com.JanCode.SKPplus.web;
 
+import com.JanCode.SKPplus.model.ActiveUsers;
+import com.JanCode.SKPplus.repository.ActiveUsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private ActiveUsersRepository activeUsersRepository;
+
     @ModelAttribute("currentUser")
     public UserDetails getCurrentUser(Authentication authentication) {
         return (authentication == null) ? null : (UserDetails) authentication.getPrincipal();
     }
+    @ModelAttribute("activeUsers")
+    public List<ActiveUsers> getActiveUsers() {
+        List<ActiveUsers> a = activeUsersRepository.getAllActiveUsers();
+        return (a == null) ? null : a;
+    }
 
     @GetMapping
     public String showAdmin() {
-
         return "/admin/admin";
     }
+
+
+
 
 
 }
--- src/main/java/com/JanCode/SKPplus/web/UserProfileController.java
+++ src/main/java/com/JanCode/SKPplus/web/UserProfileController.java
@@ -1,39 +1,26 @@
 package com.JanCode.SKPplus.web;
 
-import com.JanCode.SKPplus.model.ActiveUsers;
-import com.JanCode.SKPplus.repository.ActiveUsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/active")
-public class ActiveUsersController {
-
-    @Autowired
-    private ActiveUsersRepository activeUsersRepository;
-
+@RequestMapping("/profile")
+public class UserProfileController {
 
     @ModelAttribute("currentUser")
     public UserDetails getCurrentUser(Authentication authentication) {
         return (authentication == null) ? null : (UserDetails) authentication.getPrincipal();
     }
-    @ModelAttribute("activeUsers")
-    public List<ActiveUsers> getActiveUsers() {
-        List<ActiveUsers> a = activeUsersRepository.getAllActiveUsers();
-        //List<String>
-        return (a == null) ? null : a;
-    }
 
+//(${currentUser.getUsername()})
     @GetMapping
-    public String showActive() {
-
-        return "/admin/active";
+    public String showUserProfile() {
+        return "/user/profile";
     }
 }
