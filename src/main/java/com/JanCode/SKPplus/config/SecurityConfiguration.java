f23ce66ae492b0158360f54171d7b8f24556da48
Jan.Mazurek <mazurekjan1@gmail.com>
2021-01-10 13:52:21 +0100

    lekkie poprawki

--- src/main/java/com/JanCode/SKPplus/config/SecurityConfiguration.java
+++ src/main/java/com/JanCode/SKPplus/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.JanCode.SKPplus.authentication.MyDaoAuthenticationProvider;
 import com.JanCode.SKPplus.handler.CustomAuthenticationFailureHandler;
 import com.JanCode.SKPplus.listener.LogoutListener;
 import com.JanCode.SKPplus.listener.MyHttpSessionEventPublisher;
+import com.JanCode.SKPplus.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -28,11 +29,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public SecurityConfiguration() {
         super();
     }
-    private static final RequestMatcher PROTECTED_URLS = new OrRequestMatcher(
-            new AntPathRequestMatcher("/v1/**"),new AntPathRequestMatcher("/admin/**")
-    );
     @Autowired
-    private UserDetailsService userDetailsService;
+    private MyUserDetailsService userDetailsService;
 
     @Bean
     public HttpSessionEventPublisher httpSessionEventPublisher() {
@@ -62,8 +60,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .failureHandler(authenticationFailureHandler())
 
         .and().logout()
-                //.invalidateHttpSession(true)
-                //.clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
                 .deleteCookies("JSESSIONID")
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout");
